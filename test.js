cd453d4a14cf224d86263eccba470c414ae1d640
retecata <catalinarete@gmail.com>
2018-11-29 14:47:58 +0000

    Listing tensorflow contributors and the number of contributions

--- test.js
+++ test.js
@@ -1,19 +1,20 @@
 const octokit = require('@octokit/rest')()
 var http = require('http');
+const https = require('https');
+const request = require('request');
+const endpoint = 'https://api.github.com';
+const fetch = require('node-fetch');
 
 var github_data;
+fetch('https://api.github.com/repos/tensorflow/tensorflow/contributors')
+    .then(res => res.json())
+    .then(json =>
+      json.forEach((contributor)=>{
+        console.log(contributor.login);
+        console.log(contributor.contributions);
+      }));
 
-
-octokit.repos.getContent({
-  owner: 'retecata',
-  repo: 'CS3012',
-  path: ''
-}).then(({ data, headers, status }) => {
-  console.log(data);
-   github_data = JSON.stringify(data)
-})
-
-http.createServer(function (req, res) {
+/*http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end(github_data)
-}).listen(8080);
+}).listen(8080);*/
